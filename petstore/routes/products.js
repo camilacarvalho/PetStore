d7c59c90be94e408b26e17f2d0501c6fb4a4b1c7
camilacarvalho <camila.silva@ccc.ufcg.edu.br>
2020-03-17 18:57:25 -0300

    adicionando mais requisicoes GET

--- petstore/routes/products.js
+++ petstore/routes/products.js
@@ -3,29 +3,45 @@ var express = require('express');
 var router = express.Router();
 
 categories=[
-    {"id":1,"name":"Alimento"},
-    {"id":2, "name":"Higiene"},
-    {"id":3, "name":"Limpeza"},
+    "ALIMENTO", "HIGIENE", "ACESSORIOS"
 ];
 
 products=[
-    {"id":1,"name":"Coleira", "descrição":"Coleira na cor preta com pinjente de ossinho prata.", "avaliacao":4,"preco":30.99, "categoria":"Acessorios"},
-    {"id":2, "name":"Ração", "descrição":"Ração Canina, 18k. Para filhotes.", "avaliacao":3,"preco":100.10, "categoria":"Alimento"},
-    {"id":3, "name":"Shampoo para gatos", "descrição":"Com delicioso cheirinho de amora, o Shampoo Pet Society Beeps Estopinha Gatos Extrato de Aveia vai tornar o banho do seu gatinho prático e rápido.", "avaliacao":1,"preco":20.00, "categoria":"Higiene"}
+    {"id":1,"name":"Coleira", "description":"Coleira na cor preta com pinjente de ossinho prata.", "rating":4,"price":30.99, "category":3},
+    {"id":2, "name":"Ração", "description":"Ração Canina, 18k. Para filhotes.", "rating":3,"price":100.10, "category":1},
+    {"id":3, "name":"Shampoo para gatos", "description":"Com delicioso cheirinho de amora, o Shampoo Pet Society Beeps Estopinha Gatos Extrato de Aveia vai tornar o banho do seu gatinho prático e rápido.", "rating":1,"price":20.00, "category":2}
 ];
+
+favorites =[];
+favorites.push(products[1]);
+
 /* GET products list. */
-router.get('/products', function(req, res, next) {
+router.get('/products', function(res) {
     res.json(products);
 });
 
-router.get('/categories', function(req, res, next){
+/* GET categories list. */
+router.get('/categories', function(res){
     res.json(categories);
 });
 
-router.get('/productsByCategory/:category', function(req, res, next){
+/* GET favorites list. */
+router.get('/favorites', function(res){
+    res.json(favorites);
+});
+
+/* GET products by category list. */
+router.get('/products-by-category/:category', function(req, res){
     let category = req.params.category;
-    res.json(category);
+    productsByCategory = [];
+    var id_category = (categories.indexOf(category.toLocaleUpperCase())+1);
+    products.forEach(product => {
+        if (product.category==id_category){
+            productsByCategory.push(product)
+        }
+        
+    });
+    res.json(productsByCategory);
 });
 
 module.exports = router;
-
